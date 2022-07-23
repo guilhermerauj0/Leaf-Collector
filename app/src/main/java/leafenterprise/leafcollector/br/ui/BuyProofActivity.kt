df45348e6b0cbdba07dbb454262b892b549ecaee
arthu <arthurguilhermesb@gmail.com>
2022-07-23 18:53:32 -0300

    Add all Activity for the project with kotlin

---
+++ app/src/main/java/leafenterprise/leafcollector/br/ui/BuyProofActivity.kt
@@ -0,0 +1,13 @@
+package leafenterprise.leafcollector.br.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import leafenterprise.leafcollector.br.R
+
+
+class BuyProofActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_proof)
+    }
+}
---
+++ app/src/main/java/leafenterprise/leafcollector/br/ui/ProductActivity.java
@@ -0,0 +1,15 @@
+package leafenterprise.leafcollector.br.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import leafenterprise.leafcollector.br.R; 
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
