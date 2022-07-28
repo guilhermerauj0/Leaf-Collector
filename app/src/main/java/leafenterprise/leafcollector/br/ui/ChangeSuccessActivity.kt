9b5bc7cb5cd2dfe8218ee32cee0ef63380344129
arthu <arthurguilhermesb@gmail.com>
2022-07-28 18:52:13 -0300

    Add design of all proofAcitivity

--- app/src/main/java/leafenterprise/leafcollector/br/ui/ChangeSuccessActivity.kt
+++ app/src/main/java/leafenterprise/leafcollector/br/ui/ChangeSuccessActivity.kt
@@ -1,16 +1,30 @@
 package leafenterprise.leafcollector.br.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import leafenterprise.leafcollector.br.R
+import leafenterprise.leafcollector.br.databinding.ActivityChangeSuccessBinding
 
 class ChangeSuccessActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityChangeSuccessBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_success)
+        binding = ActivityChangeSuccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // TODO "Implementar ViewBinding"
+        binding.llProof.setOnClickListener{
+            openProof()
+        }
+
         // TODO "Implementar emissão de comprovante"
         // TODO "Implementar receber os dados da leitura do QRCODE"
+    }
+
+    private fun openProof() {
+        val intent: Intent = Intent(this, ChangeProofActivity::class.java)
+        startActivity(intent)
     }
 }
