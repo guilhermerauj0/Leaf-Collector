3f01411e159913b8949718e7299c9e6ae6b4ee49
arthu <arthurguilhermesb@gmail.com>
2022-07-30 12:21:55 -0300

    Setup Firebase Realtime Database for Login

--- app/src/main/java/leafenterprise/leafcollector/br/ui/login/RegisterActivity.java
+++ app/src/main/java/leafenterprise/leafcollector/br/ui/login/RegisterActivity.java
@@ -1,22 +1,25 @@
 package leafenterprise.leafcollector.br.ui.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import leafenterprise.leafcollector.br.databinding.ActivityRegisterBinding;
+import leafenterprise.leafcollector.br.domain.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -31,18 +34,11 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         mAuth = FirebaseAuth.getInstance();
-        String email = binding.registerEdtEmail.getText().toString();
-        String password = binding.registerEdtPassword.getText().toString();
-        String name = binding.registerEdtName.getText().toString();
 
         binding.registerBtnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(email.isEmpty() || password.isEmpty() || name.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Por favor, preencha os campos!", Toast.LENGTH_SHORT).show();
-                }else{
-                    creatAccount(email,password);
-                }
+                creatAccount();
             }
         });
     }
@@ -51,44 +47,69 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null){
+        if (currentUser != null) {
             reload();
         }
     }
-    private void creatAccount(String email, String password){
 
-        if(TextUtils.isEmpty(email)){
+    private void creatAccount() {
+
+        String email = binding.registerEdtEmail.getText().toString().trim();
+        String password = binding.registerEdtPassword.getText().toString().trim();
+        String name = binding.registerEdtName.getText().toString().trim();
+        String cpf = binding.registerEdtCpf.getText().toString().trim();
+        String phone = binding.registerEdtPhone.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
             binding.registerEdtEmail.setError("Email não pode ser vazio");
             binding.registerEdtEmail.requestFocus();
-        } else if(TextUtils.isEmpty(password)){
+
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.registerEdtEmail.setError("Insira um e-mail válido");
+            binding.registerEdtEmail.requestFocus();
+
+        } else if (TextUtils.isEmpty(password)) {
             binding.registerEdtPassword.setError("Senha não pode ser vazia");
             binding.registerEdtPassword.requestFocus();
-        }else{
+
+        } else if (TextUtils.isEmpty(name)) {
+            binding.registerEdtName.setError("Nome não pode ser vazio");
+            binding.registerEdtName.requestFocus();
+
+        } else {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Log.d(TAG, "createUserWithEmail:success");
-                                Toast.makeText(RegisterActivity.this, "Usuário criado",
-                                        Toast.LENGTH_SHORT).show();
+                            if (task.isSuccessful()) {
+                                User user = new User(name, email, cpf, phone);
+
+                                // ADD USER TO DATABASE
+                                FirebaseDatabase.getInstance().getReference("Users")
+                                        .child(mAuth.getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(RegisterActivity.this, "Usuário criado",
+                                                            Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(RegisterActivity.this, "Erro ao criar usuário",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+
                                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                updateUI(user);
                                 finish();
-                            }else{
-                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            } else {
                                 Toast.makeText(RegisterActivity.this, "Autenticação falhou",
                                         Toast.LENGTH_SHORT).show();
-                                updateUI(null);
                             }
                         }
                     });
         }
-    }
-
-    private void updateUI(FirebaseUser user) {
-    }
+        }
 
     private void reload() {
         Log.e("ERROR_CONNECTION","Usuario nao conectado");
